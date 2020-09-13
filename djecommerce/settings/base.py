a1c77ce57caecaa7f46f39b50d802a2bb408090f
Candice Han <jeeyoung.han82@gmail.com>
2020-09-13 17:57:52 +0900

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -123,9 +123,13 @@ ACCOUNT_FORMS = {
 }
 
 EMAIL_USE_TLS = True
+
 EMAIL_HOST = "smtp.gmail.com"
+
 EMAIL_PORT = 587
+
 EMAIL_HOST_USER = str(os.environ.get('EMAIL_HOST_USER', ''))
+
 EMAIL_HOST_PASSWORD = str(os.environ.get('EMAIL_HOST_PASSWORD', ''))
 
 
